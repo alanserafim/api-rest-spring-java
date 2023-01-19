c734f01ebad63edbdcfbdbef3a5bcbcd76e08ce3
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-01-18 22:44:12 -0300

    put e delete

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,29 +17,30 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
+
     @PostMapping
     @Transactional
     public void cadatrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
-    /*
-        @GetMapping
-        public List<DadosListagemMedico> listar(){
-            return repository.findAll().stream().map(DadosListagemMedico::new).toList();
-        }
-    */
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
-    /*
-        Páginação:
-        http://localhost:8080/medicos?size=1&page=3
-        Ordenação
-        http://localhost:8080/medicos?sort=nome
-        http://localhost:8080/medicos?sort=nome,desc
-        http://localhost:8080/medicos?sort=nome,desc&size=1&page=0
-     */
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+            var medico = repository.getReferenceById(dados.id());
+            medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
 
 }
